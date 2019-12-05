21473eb887c9b07e42c75c2e97074263f47006fe
MariePorquier <marieporquier@free.fr>
2019-12-05 11:53:50 +0100

    phppartie4exo2

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP partie 4 Exo 2 : Les fonctions</title>
+  </head>
+  <body>
+    <?php
+    function coucou($name){
+    //appel le résultat de la fonction
+      echo 'Coucou ' . $name . '!';
+    }
+    coucou('les folles');
+    ?>
+  </body>
+</html>
